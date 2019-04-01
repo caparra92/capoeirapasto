4f59999542fa98396201fb48852b54e91463b620
caparra92 <caparra92@gmail.com>
2019-04-01 11:25:48 -0300

    Personalizacion de login por defecto en laravel

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,44 +3,39 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-
+        <div class="col-md-5 col-md-offset-3 col-xs-10 col-xs-offset-1">
+            @if ($errors->has('email'))
+                <div class="alert alert-warning">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </div>
+            @endif
+            @if ($errors->has('password'))
+                <div class="alert alert-warning">
+                    <strong>{{ $errors->first('password') }}</strong>
+                </div>
+            @endif
+            <div class="panel panel-default form-login">
+                <h2 class="text-center">Login</h2>
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+                            <div class="col-md-12">
+                                <input id="email" type="email" class="form-control caja" name="email" value="{{ old('email') }}" required autofocus placeholder="Email">
                             </div>
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
+                            <div class="col-md-12">
+                                <input id="password" type="password" class="form-control caja" name="password" required placeholder="Password">
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                            <div class="col-md-12 text-left">
                                 <div class="checkbox">
                                     <label>
                                         <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Recordarme
@@ -48,16 +43,16 @@
                                 </div>
                             </div>
                         </div>
-
+                        <div class="text-right">
+                            <a class="btn btn-link link-green" href="{{ route('password.request') }}">
+                                Perdió su contraseña?
+                            </a>
+                        </div>
                         <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-success">
-                                    Login
+                            <div class="col-md-12">
+                                <button type="submit" class="btn btn-success btn-login">
+                                    Log in
                                 </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Perdió su contraseña?
-                                </a>
                             </div>
                         </div>
                     </form>
